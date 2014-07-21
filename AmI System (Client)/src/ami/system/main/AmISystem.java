b262a0e9f8ac99d95d28617a7215ed4e16fc8bfc
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-07-21 17:03:02 +0100

    Renamed packages

--- AmI System (Client)/src/ami/system/main/AmISystem.java
+++ AmI System (Client)/src/ami/system/main/AmISystem.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ami.system.operations.client;
+package ami.system.main;
 
-import ami.system.operations.client.menu.AmISystemMenu;
+import ami.system.operations.menu.AmISystemMenu;
 
 /**
  *
--- AmI System (Client)/src/ami/system/operations/menu/AboutApp.java
+++ AmI System (Client)/src/ami/system/operations/menu/AboutApp.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ami.system.operations.client.menu;
+package ami.system.operations.menu;
 
 /**
  *
