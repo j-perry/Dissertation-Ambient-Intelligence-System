f7b8b17d314b5fdc6eb22f0a33a773fa7aad5a92
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-08-09 15:17:57 +0100

    Now reads xml config file using XPath

--- AmI System (Client)/src/ami/system/intelligence/engine/ils/InitialMonitoringPhase.java
+++ AmI System (Client)/src/ami/system/intelligence/engine/ils/InitialMonitoringPhase.java
@@ -7,6 +7,9 @@ import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.*;
 
 /**
@@ -38,50 +41,63 @@ public class InitialMonitoringPhase {
      */
     public void run() {
         String startDate = null,
-               currentDate = null;
+                currentDate = null;
         final String fileName = "projects/AmI_System/xml/ami_config.xml";
         date = new Date();
 
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
         currentDate = df.format(date).toString();
 
-        // XML File
-        initialMonitoringPhaseConfig = new File(fileName);
-        
         try {
+            initialMonitoringPhaseConfig = new File(fileName);
             xmlBuilderFactory = DocumentBuilderFactory.newInstance();
             xmlBuilder = xmlBuilderFactory.newDocumentBuilder();
             Document doc = xmlBuilder.parse(initialMonitoringPhaseConfig);
-            NodeList parentNode = doc.getElementsByTagName("initial-monitoring");
-
-            // we only have one level in our XML file
-            Node childrenNodes = parentNode.item(0);
-
-            if (childrenNodes.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) parentNode;
-                startDate = element.getElementsByTagName("startDate").item(0).getTextContent();
+            
+            XPathFactory pathFactory = XPathFactory.newInstance();
+            XPath path = pathFactory.newXPath();
+            startDate = (String) path.evaluate("config/initial-monitoring/startDate", doc);
+            
+            if(startDate.isEmpty()) {
+                System.out.println("EMPTY");
+            } else {
+                System.out.println(startDate);
             }
+            
+//            NodeList parentNode = doc.getElementsByTagName("initial-monitoring");
+            
+//            // we only have one level in our XML file
+//            Node childrenNodes = parentNode.item(1);
+//            
+////            if (childrenNodes.getNodeType() == Node.ELEMENT_NODE) {
+//                Element element = (Element) parentNode;
+//                startDate = element.getElementsByTagName("startDate").item(0).getTextContent();
+////            }
+//            
+//            /*
 
+            /*
             // if the 'start-date' element is empty...
             if (startDate.equals("")) {
                 // set the element...
                 Element element = (Element) parentNode;
-                element.appendChild(doc.createTextNode(startDate) );
-                
+                element.appendChild(doc.createTextNode(startDate));
+
                 // and write it...
                 TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new File(fileName));
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File(fileName));
                 transformer.transform(source, result);
-                
+
                 // then run the initial monitoring phase                
                 generateSaturatedModel();
                 generateUnsaturatedModel();
-                
+
             } else if (startDate.equals(currentDate)) {
                 return;
-            }            
+            }
+            */
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -91,7 +107,6 @@ public class InitialMonitoringPhase {
      *
      */
     public void identifyContext() {
-        
     }
 
     /**
