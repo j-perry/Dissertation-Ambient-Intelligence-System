4bb3e522c13aceeacf7112960200818a8dbf6441
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-08-13 10:55:01 +0100

    Writes initial context data to database

--- AmI System (Client)/src/ami/system/intelligence/engine/isl/behaviours/FuzzyLogicController.java
+++ AmI System (Client)/src/ami/system/intelligence/engine/isl/behaviours/FuzzyLogicController.java
@@ -1,5 +1,7 @@
 package ami.system.intelligence.engine.isl.behaviours;
 
+import ami.system.operations.resources.database.InitialMonitoringDB;
+
 /**
  * Based on implementation in
  * http://www.researchgate.net/profile/Luis_Magdalena/publication/223851736_A_Fuzzy_logic_controller_with_learning_through_the_evolution_of_its_knowledge_base/links/0912f50a37e31dc51d000000
@@ -11,12 +13,13 @@ public class FuzzyLogicController {
     private DataBase dataBase;
     private RuleBase ruleBase;
     private DecisionTree decisionTree;
-    
+    private InitialMonitoringDB dbInitial;
 
     public FuzzyLogicController() {
         dataBase = new DataBase();
         ruleBase = new RuleBase();
-        decisionTree = new DecisionTree();
+        decisionTree = new DecisionTree();        
+        dbInitial = new InitialMonitoringDB();
     }
 
     /**
@@ -38,7 +41,8 @@ public class FuzzyLogicController {
      * @param minute
      */
     public void persist(int hour, int minute) {
-        
-        
+        dbInitial.open();
+        dbInitial.persist(hour, minute, dataBase.getValue(), dataBase.getType(), dataBase.getLinguisticType());
+        dbInitial.close();
     }
 }
---
+++ AmI System (Client)/src/ami/system/operations/resources/database/InitialMonitoringDB.java
@@ -0,0 +1,115 @@
+
+
+
+package ami.system.operations.resources.database;
+
+import static ami.system.operations.resources.database.IDatabase.*;
+import java.sql.*;
+
+/**
+ *
+ * @author Jonathan Perry
+ */
+public class InitialMonitoringDB implements IDatabase {
+    
+    private String query;
+    
+    private Statement qryStatement;
+    private PreparedStatement prepQuery;
+    private ResultSet resultSet;
+    private Connection conn;
+    
+    public InitialMonitoringDB() {
+        // register the driver
+        try {
+            Class.forName(driver);
+        }
+        catch(ClassNotFoundException ex) {
+            // if not found
+            ex.printStackTrace();
+        }
+    }   
+    
+    /**
+     * Open a JDBC connection
+     */
+    @Override
+    public void open() {
+        try {
+            // https://mysql.student.sussex.ac.uk/phpmyadmin/
+            conn = DriverManager.getConnection(dbUrl, username, password);
+            
+            // create the table if necessary
+            createTable();
+            System.out.println("> Connection has been found");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Close the JDBC connection
+     */
+    @Override
+    public void close() {
+        try {
+            conn.close();
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+        
+    /**
+     * Creates a table if it doesn't exist
+     */
+    private void createTable() {
+        String tableName = "initial";
+        query = "CREATE TABLE IF NOT EXISTS " + tableName + " " +
+                "( " +
+                "  Id             INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+                "  Hour           INTEGER, " + 
+                "  Minute         INTEGER, " + 
+                "  Value          INTEGER, " + 
+                "  Context        VARCHAR(20), " + 
+                "  LinguisticType VARCHAR(30) " + 
+                ")";
+        
+        int status = 0;
+         
+        try {
+            qryStatement = conn.createStatement();
+            status = qryStatement.executeUpdate(query);
+        }
+        catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    } 
+    
+    /**
+     * 
+     */
+    public void persist(int hour, int minute, int value, String context, String linguisticType) {
+        query = "INSERT INTO initial (Hour, Minute, Value, Context, LinguisticType)" +
+                "VALUES ('" + hour + "' ," +
+                        "'" + minute + "' ," +
+                        "'" + value + "' ," +
+                        "'" + context + "' ," +
+                        "'" + linguisticType + "')";
+        int status = 0;
+                
+        // create the database table, if it doen't exist
+        
+        
+        try {
+            qryStatement = conn.createStatement();
+            status = qryStatement.executeUpdate(query);
+            
+            System.out.println("> Data has been written");
+            System.out.println();
+        }
+        catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
